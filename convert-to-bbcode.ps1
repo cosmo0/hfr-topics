632b26d6e4af4304dc3a25ecad73552ae5f54852
cosmo0 <thomas.broust@gmail.com>
2021-04-06 10:14:25 +0200

    Conversion script continued

--- convert-to-bbcode.ps1
+++ convert-to-bbcode.ps1
@@ -1,20 +1,15 @@
-# url HFR : \[url=([^\]]+)\]([^\[]+)\[/url\]
-
 # WARNING: for now only a smaller conversion set is implemented, just for the news
 
 $file = $Args[0]
-$content = get-content $file -raw
+$content = get-content $file -raw -Encoding UTF8
 
 # titles
-
 $content = $content -replace '## (.*)',"`n[fixed]`$1[/fixed]"
 
 # bold
-
 $content = $content -replace '\*\*([^\*]+)\*\*','[b]$1[/b]'
 
 # save result
-
-$content | out-file 'result.txt'
+$content | out-file 'result.bb'
 
 echo "done"
