9587d03f30495397e9bbb0329021725e3f2a42d5
Thomas Broust <thomas.broust@gmail.com>
2021-04-26 00:41:53 +0200

    Conversion des URL

--- convert-to-bbcode.ps1
+++ convert-to-bbcode.ps1
@@ -12,6 +12,9 @@ $content = $content -replace '\*\*([^\*]+)\*\*','[b]$1[/b]'
 # images
 $content = $content -replace '!\[[^\]]*\]\((.*)\)', '[img]$1[/img]'
 
+# links
+$content = $content -replace '\[([^\]]*)\]\((.*)\)', '[url=$2]$1[/url]'
+
 # save result
 $content | out-file 'result.bb'
 
